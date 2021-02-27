30a6be9bb0006fb648699bc40358146c7f10d643
pid011 <pid011@kakao.com>
2021-02-27 21:25:17 +0900

    Add Clock ui

---
+++ src/MaplestoryClockWidget/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using MaplestoryClockWidget.Helpers;
+
+using System;
+using System.Windows.Threading;
+
+namespace MaplestoryClockWidget.ViewModels
+{
+    public class MainWindowViewModel : Observable
+    {
+        private string _time;
+        private bool _flagTimeVisible;
+
+        public string Time
+        {
+            get => _time;
+            set => Set(ref _time, value);
+        }
+
+        public bool FlagTimeVisible
+        {
+            get => _flagTimeVisible;
+            set => Set(ref _flagTimeVisible, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            var timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(100)
+            };
+            timer.Tick += Timer_Tick;
+
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Time = DateTime.Now.ToString("tt hh시 mm분 ss초");
+        }
+    }
+}
---
+++ src/MaplestoryClockWidget/Views/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using MaplestoryClockWidget.ViewModels;
+
+using System.Windows;
+using System.Windows.Input;
+
+namespace MaplestoryClockWidget.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
